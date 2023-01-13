79050cc04316d8ea2461d0b9d16828513b9b0438
Christyan Basante <cbasante@unipacifico.edu.co>
2023-01-13 14:08:02 -0500

    Modificated styles.css, pulled code html of some page to index file; modificated main.js file: merged email-nav-bar with its menu, mobile-menu too

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const asideMyOrder = document.querySelector('.product-detail');
 
-menuEmail.addEventListener('click', toggleDesktopMenu, true);
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
-    console.log ('click');
+    const isAsideClosed = asideMyOrder.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        asideMyOrder.classList.add('inactive');
+    };
     desktopMenu.classList.toggle('inactive');
 };
-console.log('js funcionando');
+
+function toggleMobileMenu() {
+    const isAsideClosed = asideMyOrder.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        asideMyOrder.classList.add('inactive');
+    };
+    menuMobile.classList.toggle('inactive');
+};
+
+function toggleShoppingCart() {
+    const isMenuMobileClosed = menuMobile.classList.contains('inactive');
+    const isEmailMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMenuMobileClosed || !isEmailMenuClosed) {
+        menuMobile.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    asideMyOrder.classList.toggle('inactive');
+};
